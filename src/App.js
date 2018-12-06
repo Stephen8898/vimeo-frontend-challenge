72c984def386e4d6a4776979d6c7759780a5d2d8
Stephen Sarfo <Stephen.sarfo1998@outlook.com>
2018-12-06 08:44:55 -0500

    inital commit

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+
+
+
+class App extends Component {
+  render() {
+
+    return (
+    <React.Fragment>
+      <div className="container" style={{marginTop:'10%', marginBottom:'10%'}}>
+        <div className="row align-items-center" >
+        <div className="col-md">
+            <img  src="assets/vimeo-pic3.jpg" alt="picture Monsoon III" />
+          </div>
+          <div className="col-6">
+              <h3>Monsoon III</h3>
+              <p>Isn't that fantastic? You can just push a little tree out of your brush like that. 
+                 You don't have to spend all your time thinking about what you're doing, you just 
+                 let it happen. A fan brush is a fantastic piece of equipment. Use it. Make friends 
+                 with it. Let your imagination be your guide. Just let your mind wander and enjoy. 
+                 </p>
+          </div>
+        </div>
+      </div>
+      <div className="container-fluid" style={{backgroundImage:'linear-gradient(to bottom, rgb(55, 55, 56,1), rgb(55, 55, 56, 0))'}}>
+      <div className="container" style={{paddingTop:'10%'}}>
+      <div className="row align-items-center" >
+        <div className="item2">
+          <div className="col-6" style={{Color:'#fffff'}}>
+              <h3>Beams</h3>
+              <p>Isn't that fantastic? You can just push a little tree out of your brush like that. 
+                 You don't have to spend all your time thinking about what you're doing, you just 
+                 let it happen. A fan brush is a fantastic piece of equipment. Use it. Make friends 
+                 with it. Let your imagination be your guide. Just let your mind wander and enjoy. 
+                 </p>
+          </div>
+          <div className="col-md">
+            <img  src="assets/vimeo-pic2.jpg" alt="picture Monsoon III" />
+          </div>
+        </div> 
+          <div className="container" style={{paddingTop:'10%'}}>
+          <div className="col-sm">
+            <img  src="assets/vimeo-pic1.jpg" alt="picture Monsoon III" />
+          </div>
+          <div className="col-6" style={{Color:'#fffff'}}>
+              <h3>Move 2</h3>
+              <p>Isn't that fantastic? You can just push a little tree out of your brush like that. 
+                 You don't have to spend all your time thinking about what you're doing, you just 
+                 let it happen. A fan brush is a fantastic piece of equipment. Use it. Make friends 
+                 with it. Let your imagination be your guide. Just let your mind wander and enjoy. 
+                 </p>
+          </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </React.Fragment>
+    );
+  }
+}
+
+export default App;
