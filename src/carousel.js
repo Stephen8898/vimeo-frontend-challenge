8de44f33bbc9a668563ddeb51f6d3da744aed6c7
Stephen Sarfo <Stephen.sarfo1998@outlook.com>
2018-12-11 22:44:06 -0500

    Fixed carousel layou, deleted app.js

--- src/carousel.js
+++ src/carousel.js
@@ -2,41 +2,41 @@ import React, { Component } from 'react'
 import Layout from './components/layout.js'
 import './style/layout.css'
 
+let newIndex = 0;
+
 export default class carousel extends Component {
-  state = {
-    images: [],
-    title:[],
-    descriptions: []
+ state = {
+      index : 0,
   }
-
-  id = [304230331]
-componentDidMount(){
-  fetch(`https://api.vimeo.com/videos/${this.id[0]}`)
-    .then((result) => {
-      return result.json();
-    })
-    .then((json) => {
-      return console.log(json)
-    })
-}
-
+         
+  
 next = () => {
-
+ let num = newIndex++;
+  this.update(num)
 }
-
 prev = () => {
-
+  
+  let num = newIndex--;
+  this.update(num);
 }
+update(newIndex){
+let states = this.setState({
+   index: newIndex
+ })
+
+ return states;
+ 
+}
+
+  imgs = ["assets/vimeo-pic1.jpg","assets/vimeo-pic2.jpg","assets/vimeo-pic3.jpg"]
+  title = ["Move 2","Beams", "Monsoon III"]
 
   render() {
 
-    let images = this.props.images
-    let descriptions = this.props.descriptions;
 
-    var {items} = this.state;
     return (
   <React.Fragment>
-    <div className="backgroundImg" style={{backgroundColor:'grey', backgroundImage: `url(${images})`, backgroundRepeat:'no-repeat', backgroundSize:'cover', filter:'blur(8px)'}}></div>
+    <div className="backgroundImg" style={{backgroundColor:'grey', backgroundImage: `url(${this.imgs[this.state.index]})`, backgroundRepeat:'no-repeat', backgroundSize:'cover', filter:'blur(8px)'}}></div>
     <div className="jumbotron-fluid" style={{ marginTop:'2%', marginBottom:'5%'}}>
       <div className="container">
         <div id="carouselExampleControls" className="carousel slide" data-ride="carousel">
@@ -45,30 +45,30 @@ prev = () => {
               <div className="container">
                 <div className="row justify-content-center">
                     <div className="col-4 text-center">
-                      <img className="img-fluid" src="assets/vimeo-pic2.jpg" alt="poster 1" height="350px" width="250px"/>
+                      <img className="img-fluid" src={this.imgs[this.state.index]} alt="poster 1" height="350px" width="250px"/>
                    </div>
                    <div className="col-5 align-middle" style={{color:'white'}}>
-                      <h3>Move 2</h3>
+                      <h3>{this.title[this.state.index]}</h3>
                       <p >Isn't that fantastic? You can just push a little tree out of your brush like that. 
                          You don't have to spend all your time thinking about what you're doing, you just 
                           let it happen. A fan brush is a fantastic piece of equipment. Use it. Make friends 
                         </p>
                     </div>
                 </div>
-                <div className="row justify-content-lg-around"> 
+                <div className="row justify-content-around"> 
                   <div className="col-4 text-right">
-                    <button className="btn btn-primary" type="button" style={{marginRight:'1rem'}}>Buy Now</button>
-                    <button className="btn btn-outline-dark" type="button" style={{marginLeft:'1rem', color:'white'}}>Watch Trailer</button>
+                    <button className="btn btn-primary" type="button" >Buy Now</button>
+                    <button className="btn btn-outline-dark" type="button" style={{marginLeft:'.5rem', color:'white'}}>Watch Trailer</button>
                   </div>
                 </div>
                   </div>
-                    <a class="carousel-control-prev"  role="button" data-slide="prev">
-                      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                      <span class="sr-only">Previous</span>
+                    <a className="carousel-control-prev" onClick={this.prev} role="button" data-slide="prev">
+                      <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                      <span className="sr-only">Previous</span>
                     </a>
-                    <a class="carousel-control-next"  role="button" data-slide="next">
-                      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                      <span class="sr-only">Next</span>
+                    <a className="carousel-control-next" onClick={this.next} role="button" data-slide="next">
+                      <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                      <span className="sr-only">Next</span>
                     </a>
                 </div>
               </div>
--- src/components/layout.js
+++ src/components/layout.js
@@ -4,7 +4,7 @@ export default class layout extends Component {
   render() {
     return (
         <React.Fragment>
-        <div className="container" style={{marginTop:'10%', marginBottom:'10%'}}>
+        <div className="container" style={{marginTop:'15%', marginBottom:'10%'}}>
           <div className="row" >
           <div className="col" >
               <img  src="assets/vimeo-pic3.jpg" alt="picture Monsoon III" style={{width:'100%'}}/>
@@ -21,9 +21,8 @@ export default class layout extends Component {
       </div>
         <div className="container-fluid" style={{backgroundImage:'linear-gradient(to bottom, rgb(55, 55, 56,1), rgb(55, 55, 56, 0))'}}>
         <div className="container" style={{paddingTop:'10%'}}>
-        <div className="row align-items-center" >
-          <div className="item2">
-            <div className="col-6" style={{Color:'#fffff'}}>
+        <div className="row" > 
+            <div className="col" style={{color:'#ffff'}}>
                 <h3>Beams</h3>
                 <p>Isn't that fantastic? You can just push a little tree out of your brush like that. 
                    You don't have to spend all your time thinking about what you're doing, you just 
@@ -31,24 +30,25 @@ export default class layout extends Component {
                    with it. Let your imagination be your guide. Just let your mind wander and enjoy. 
                    </p>
             </div>
-            <div className="col-md">
+            <div className="col">
               <img  src="assets/vimeo-pic2.jpg" alt="picture Beams" style={{width:'100%'}}/>
             </div>
           </div> 
-            <div className="container" style={{paddingTop:'10%'}}>
-            <div className="col-sm">
+      <div className="container" style={{paddingTop:'10%'}}>
+        <div className="row"> 
+            <div className="col">
               <img  src="assets/vimeo-pic1.jpg" alt="picture Move 2" style={{width:'100%'}} />
             </div>
-            <div className="col-6" style={{Color:'#fffff'}}>
+             <div className="col" style={{color:'#ffff'}}>
                 <h3>Move 2</h3>
                 <p>Isn't that fantastic? You can just push a little tree out of your brush like that. 
                    You don't have to spend all your time thinking about what you're doing, you just 
                    let it happen. A fan brush is a fantastic piece of equipment. Use it. Make friends 
                    with it. Let your imagination be your guide. Just let your mind wander and enjoy. 
                    </p>
+                </div>
+            </div> 
             </div>
-            </div>
-          </div>
         </div>
       </div>
     </React.Fragment>
